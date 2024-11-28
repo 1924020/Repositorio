8efbec3ae45cc1644855037082db8836f7ba70f1
Syd <1924020@alu.murciaeduca.es>
2024-11-28 17:06:25 +0100

    Update supervisar.sh

--- supervisar.sh
+++ supervisar.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 LOCKFILE="/tmp/monitorizacion_supervisar.lock"
-# Dirección de correo para las alertas
 ALERT_EMAIL="nohomothoo@gmail.com"
-TEMP_FILE="/tmp/monitoreo_ps_output.txt"  # El archivo temporal donde monitoreo_ps.sh guarda su salida
-umask 000
+TEMP_FILE="/tmp/monitoreo_ps_output.txt"  # El archivo temporal donde monitoreo_ps.sh guarda su salida 
+
+umask 000 #Prevenir problemas de permisos
 
 send_alert() {
     local message="$1"
     local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-    local subject="ALERTA CRÍTICA [$timestamp]"
+    local subject="ALERTA CRÍTICA EN EL SERVIDOR[$timestamp]"
 
     # Usamos un archivo temporal para crear el correo con los encabezados
     (echo "Subject: $subject"
@@ -32,9 +33,9 @@ trap 'rm -f "$LOCKFILE"' EXIT
 # Ejecutar monitoreo_ps.sh
 bash /home/monitorizacion/monitoreo_ps.sh
 
-# Verificar si el archivo temporal existe (esto indica que se ejecutó desde supervisar.sh)
+# Verificar si el archivo temporal existe (creado desde el script de monitorizacion principal)
 if [ -f "$TEMP_FILE" ]; then
-    # Leer el archivo temporal que contiene las alertas y verificar si hay alertas críticas
+    # Leer el archivo temporal que contiene las alertas y verificar si hay alertas críticas que hayan sido registrados con logcheck
     critical_alerts=$(grep -i "CRITICAL" "$TEMP_FILE")
     
     if [ -n "$critical_alerts" ]; then
@@ -47,5 +48,6 @@ if [ -f "$TEMP_FILE" ]; then
     # Borrar el archivo temporal después de enviarlo (para evitar que se acumule)
     rm -f "$TEMP_FILE"
 else
-    echo "No se generó el archivo temporal. Asegúrate de que monitoreo_ps.sh se ejecute correctamente."
+    #Control de errores, debug al ejecutar
+    echo "No se generó el archivo temporal. Asegúrate de que monitoreo_ps.sh se ejecute correctamente." 
 fi
